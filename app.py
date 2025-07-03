aa1308aee7cbd2dfb5ba916ea4fb8a5860287e83
Nishu Mahato <kreationnisu123@gmail.com>
2025-07-03 19:51:47 +0545

    Update app.py to bind on 0.0.0.0 and use PORT env variable for Render deployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+import os
 
 app = Flask(__name__)
 
@@ -12,5 +13,6 @@ def home():
 def about():
     return render_template('about.html')
 
-if __name__=="__main__":
-  app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
